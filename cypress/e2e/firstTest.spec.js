3b2baf73fec036a66c3d577b9bd36a9305c04691
degemar.silva <degemar.silva@sensedata.com.br>
2023-10-25 20:34:51 -0300

    saving the subjects of the command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -11,10 +11,26 @@ describe('First test suite', () => {
     cy.get('[data-cy="imputEmail1"]')
   })
 
-  it.only('Second test', () => {
+  it('Second test', () => {
     cy.contains('[status="primary"]', 'Sign in')
     cy.get('nav.fixed')
       .find('g[data-name="menu-2"]')
       .click()
+  })
+
+  it.only('Save subject of the command', () => {
+    cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+    cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+    // Cypress alias
+    cy.contains('nb-card', 'Using the Grid').as('usingTheGrid')
+    cy.get('@usingTheGrid').find('[for="inputEmail1"]').should('contain', 'Email')
+    cy.get('@usingTheGrid').find('[for="inputPassword2"]').should('contain', 'Password')
+
+    // Cypress then()
+    cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
+      cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
+      cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+    })
   })
 })
