b6b4dd684d74ca6ea9a320e0960d527bf99edb00
degemar.silva <degemar.silva@sensedata.com.br>
2023-10-28 21:04:20 -0300

    extraindo texto de elemetos web e adicionando novo script

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -18,7 +18,7 @@ describe('First test suite', () => {
       .click()
   })
 
-  it.only('Save subject of the command', () => {
+  it('Save subject of the command', () => {
     cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
     cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
 
@@ -31,6 +31,33 @@ describe('First test suite', () => {
     cy.contains('nb-card', 'Using the Grid').then(usingTheGridForm => {
       cy.wrap(usingTheGridForm).find('[for="inputEmail1"]').should('contain', 'Email')
       cy.wrap(usingTheGridForm).find('[for="inputPassword2"]').should('contain', 'Password')
+    })
+  })
+
+  it.only('Extract text value', () => {
+    // 1
+    cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+    // 2
+    cy.get('[for="exampleInputEmail1"]').then( label => {
+      const labelTex = label.text()
+      expect(labelTex).to.equal('Email address')
+    })
+
+    // 3
+    cy.get('[for="exampleInputEmail1"]').invoke('text').then( text => {
+      expect(text).to.equal('Email address')
+    })
+
+    // 4
+    cy.get('[for="exampleInputEmail1"]').invoke('attr', 'class').then( classValue => {
+      expect(classValue).to.equal('label')
+    })
+
+    // 5
+    cy.get('#exampleInputEmail1').type('test@test.com')
+    cy.get('#exampleInputEmail1').invoke('prop', 'value').should('contain', 'test@test.com').then( prop => {
+      expect(prop).to.equal('test@test.com')
     })
   })
 })
