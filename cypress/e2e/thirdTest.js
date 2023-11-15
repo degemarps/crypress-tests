e93872301bb153736a9b86c66e3c9e0886abdf3b
degemar.silva <degemar.silva@sensedata.com.br>
2023-11-15 15:42:38 -0300

    trabalhando com filtros em tabelas

--- cypress/e2e/thirdTest.js
+++ cypress/e2e/thirdTest.js
@@ -14,7 +14,7 @@ describe('Third test suite', () => {
       cy.wrap(tableRow).find('td').eq(6).should('contain', '35')
     })
 
-    //get the row by index
+    // get the row by index
     cy.get('thead').find('.nb-plus').click()
     cy.get('thead').find('tr').eq(2).then( tableRow => {
       cy.wrap(tableRow).find('[placeholder="First Name"]').type('Harry')
@@ -27,5 +27,19 @@ describe('Third test suite', () => {
       cy.wrap(tableColumns).eq(3).should('contain', 'Potter')
     })
 
+    // get each row validation
+    const age = [20, 30, 40, 200]
+
+    cy.wrap(age).each( age => {
+      cy.get('thead [placeholder="Age"]').clear().type(age)
+      cy.wait(500)
+      cy.get('tbody tr').each( tableRow => {
+        if (age == 200){
+          cy.wrap(tableRow).should('contain', 'No data found')
+        } else {
+          cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+        }
+      })
+    })
   })
 })
