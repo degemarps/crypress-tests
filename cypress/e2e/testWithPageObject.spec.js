6e32afc7cd31cb4c870e9f3ed01368894557d9e6
degemar.silva <degemar.silva@sensedata.com.br>
2023-12-22 16:39:10 -0300

    preenchimento de formulário usando page objects e cria comando cypress

--- cypress/e2e/testWithPageObject.spec.js
+++ cypress/e2e/testWithPageObject.spec.js
@@ -1,12 +1,19 @@
-import { navigateTo } from "../support/navigationPages"
+import { onFormLayoutsPage } from "../support/page Objects/formLayoutsPage"
+import { navigateTo } from "../support/page Objects/navigationPages"
 
 describe('Suite tests with page objects', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.openHomePage()
   })
 
   it('Visit pages', () => {
     navigateTo.formLayoutsPage()
     navigateTo.datepickerPage()
+  })
+
+  it('should submit inline and basic form and select tomorrow date in the calendar', () => {
+    navigateTo.formLayoutsPage()
+    onFormLayoutsPage.submitInlineFormWithNameAndEmail('degemar', 'test@test.com')
+    onFormLayoutsPage.submitBasicFormWithEmailAndPassword('test@test.com', 'test123')
   })
 })
